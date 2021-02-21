48e2689e76b2c540b6d5884059618037a491b1e3
Sinem Oyan <sinem.oyan@cuztranslation.com>
2021-02-21 13:55:11 -0500

    employee validation, update, delete

--- src/main/java/net/javaci/javacibank/backoffice/controller/EmployeeController.java
+++ src/main/java/net/javaci/javacibank/backoffice/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package net.javaci.javacibank.backoffice.controller;
 
 
 
+
+
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +12,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import net.javaci.javacibank.backoffice.dto.EmployeeDto;
@@ -34,7 +40,7 @@ public class EmployeeController {
 	public String renderAddPage(Model model) {
 		EmployeeDto employeeDto = new EmployeeDto();
 		
-		employeeDto.setCitizenNumber("1234");
+		// employeeDto.setCitizenNumber("1234");
 		
 		model.addAttribute("employeeDto", employeeDto);
 		
@@ -42,13 +48,47 @@ public class EmployeeController {
 	}
 	
 	@PostMapping("/add")
-	public String addEmployee(@ModelAttribute EmployeeDto employeeDto) {
+	public String addEmployee(@ModelAttribute @Validated EmployeeDto employeeDto, BindingResult bindingResult, Model model) {
+		
+		if (bindingResult.hasErrors() || !employeeDto.getConfirmPassword().equals(employeeDto.getPassword())) {
+			model.addAttribute("errorMessage", bindingResult.getAllErrors().get(0).getDefaultMessage());
+			return "error/javascriptValidationIgnored";
+		}
 		
 		Employee employee = new Employee();
 		modelMapper.map(employeeDto, employee);
 		
-		employeeDao.add(employee);
+		employeeDao.save(employee);
 		
+		return "redirect:/employee/list";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String renderUpdatePage(Model model, @PathVariable("id") Long id) {
+		
+		Employee employeeEntity = employeeDao.findById(id).get();
+		EmployeeDto employeeDto = new EmployeeDto();
+
+		modelMapper.map(employeeEntity, employeeDto);
+
+		model.addAttribute("employeeDto", employeeDto);
+
+		return "employee/update";
+	}
+
+	@PostMapping("/update/{id}")
+	public String handleUpdate(@ModelAttribute @Validated EmployeeDto employeeDto, BindingResult bindingResult,
+			Model model, @PathVariable("id") Long id) {
+
+		if (bindingResult.hasErrors()) {
+			return "error/javaScriptValidationIgnored";
+		}
+
+		Employee employeeEntity = employeeDao.findById(id).get();
+		modelMapper.map(employeeDto, employeeEntity);
+		
+		employeeDao.save(employeeEntity);
+
 		return "redirect:/employee/list";
 	}
 	
@@ -64,6 +104,25 @@ public class EmployeeController {
 			model.addAttribute("date_format", "dd-MM-yyyy");
 		}
 		return "employee/list";
+	}
+	
+	@PostMapping("/delete/{id}")
+	public String handleUpdate(@PathVariable("id") Long id) {
+		
+		if (employeeDao.existsById(id) == false) {
+			return "error/404";
+		}
+		
+		employeeDao.deleteById(id);
+		
+		return "redirect:/employee/list";
+	}
+
+	
+	@GetMapping("/checkCitizenNumber/{citizenNumber}")
+	@ResponseBody
+	public boolean checkCitizenNumber(@PathVariable("citizenNumber") String citizenNumber) {
+		return employeeDao.existsByCitizenNumber(citizenNumber);
 	}
 	
 }
--- src/main/java/net/javaci/javacibank/db/model/Employee.java
+++ src/main/java/net/javaci/javacibank/db/model/Employee.java
@@ -2,6 +2,9 @@ package net.javaci.javacibank.db.model;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Getter;
@@ -13,15 +16,20 @@ import net.javaci.javacibank.db.model.enumaration.EmployeeStatusType;
 @Getter @Setter @ToString
 public class Employee {
 	private Long id;
-
+//buradaki butun degiskenler  icin databasede alan olmali
     private String citizenNumber;
+    
+    @NotEmpty
+    @Size(min = 2, max = 32)
     
     private String firstName;
     
     private String middleName;
-
+    @NotEmpty
+    @Size(min = 2, max = 32)
     private String lastName;    
-    
+    @NotEmpty
+    @Size(min = 2, max = 64)
     private String email;
     
     @DateTimeFormat(pattern = "yyyy-MM-dd")
